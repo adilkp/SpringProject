e3a55b23b04e4a57f97d32e9b85af5dd102d3d2c
Adil Pathan <emb-adilpat@emb-ban-184.embitel.local>
2019-10-10 14:52:41 +0530

    H2 Database connectivity established and products added successfully

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -3,18 +3,21 @@ package com.emusicstore.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.emusicstore.dao.ProductDao;
+import com.emusicstore.dao.impl.ProductDaoImpl;
 import com.emusicstore.model.Product;
 
 @Controller
 public class HomeController {
 	
-	private  ProductDao productDao = new ProductDao();
+	@Autowired
+	private ProductDao productDao;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -24,7 +27,7 @@ public class HomeController {
 	@RequestMapping("/productList")
 	public String getProducts(Model model) {
 		
-		List<Product> products  = productDao.getProductList();
+		List<Product> products  = productDao.getAllProducts();
 		model.addAttribute("products",products);
 		return "productList";
 	}
--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -1,67 +1,17 @@
 package com.emusicstore.dao;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.emusicstore.model.Product;
 
-public class ProductDao {
+public interface ProductDao {
 	
-	private List<Product> productList;
+	void addProduct(Product product);
 	
-	public List<Product> getProductList() {
-	Product p1 = new Product();
-	p1.setProductId("p123");
-	p1.setProductName("Gtx2080Ti");
-	p1.setProductCategory("Graphics card");
-	p1.setProductDescription("Graphics card based on turing architecture haveing 11 GB RAM");
-	p1.setProductPrice(1300);
-	p1.setProductCondition("Latest");
-	p1.setProductStatus("Active");
-	p1.setUnitInStock(786);
-	p1.setProductManufacturer("Nvdia");
+	Product getProductById(String id);
 	
-	Product p3 = new Product();
-	p3.setProductId("p125");
-	p3.setProductName("Gtx2080");
-	p3.setProductCategory("Graphics card");
-	p3.setProductDescription("Graphics card based on turing architecture having 8 GB RAM");
-	p3.setProductPrice(900);
-	p3.setProductCondition("Latest");
-	p3.setProductStatus("Active");
-	p3.setUnitInStock(313);
-	p3.setProductManufacturer("Nvdia");
+	List<Product> getAllProducts();
 	
-	Product p2 = new Product();
-	p2.setProductId("p786");
-	p2.setProductName("Gtx2060");
-	p2.setProductCategory("Graphics card");
-	p2.setProductDescription("Graphics card based on turing architecture having 6 GB RAM");
-	p2.setProductPrice(500);
-	p2.setProductCondition("Latest");
-	p2.setProductStatus("Active");
-	p2.setUnitInStock(195);
-	p2.setProductManufacturer("Nvdia");
-	
-	productList = new ArrayList<Product>();
-	productList.add(p1);
-	productList.add(p2);
-	productList.add(p3);
-	
-	return productList;
-	
-	}
-
-	public Product getProductById(String productId) throws IOException {
-		for(Product product: getProductList()) {
-			if(product.getProductId().equals(productId)) {
-				return product;
-			}
-		}
-		throw new IOException("No product found");
-	}
-	
-    
+	void deleteProduct(String id);
 	
 }
